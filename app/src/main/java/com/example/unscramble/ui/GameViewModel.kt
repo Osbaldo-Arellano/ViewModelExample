13c9e89bfb9c79bc88383d7d6dd851746ad25619
Osbaldo-Arellano <osbaldoarellano1996@gmail.com>
2025-02-02 23:12:00 -0800

    displaying scrambled words

---
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,50 @@
+package com.example.unscramble.ui
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import com.example.unscramble.data.allWords
+
+
+class GameViewModel : ViewModel()  {
+
+    private val _uiState = MutableStateFlow(GameUiState())
+
+    // Read-only State that other classes can get
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+
+    private lateinit var currentWord: String
+
+    private var usedWords: MutableSet<String> = mutableSetOf()
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+        if(usedWords.contains(currentWord)) {
+            return pickRandomWordAndShuffle()
+        } else {
+            usedWords.add(currentWord)
+            return shuffleCurrentWord(currentWord)
+        }
+    }
+
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+
+        tempWord.shuffle()
+
+        while (String(tempWord) == word){
+            tempWord.shuffle()
+        }
+
+        return String(tempWord)
+    }
+
+    private fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
+
+    init {
+        resetGame()
+    }
+}
